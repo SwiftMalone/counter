3ca9c974a95fe04ed577fe71fa5cb8bdec3eaa80
Rommaash <Rommaash@yandex.ru>
2024-06-28 18:12:45 +0300

    Создан проект

---
+++ counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counter
+//
+//  Created by Роман Абеленцев on 28/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
